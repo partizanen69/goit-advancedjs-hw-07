d357678a83720b19f5652491c0747446fc77c78d
Oleksii Ablitsov <oleksii.ablitsov@bringg.com>
2023-12-05 18:39:49 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,45 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return key;
+  }
+}
+
+abstract class House {
+  private tenants: Person[] = [];
+  public door: boolean = false;
+
+  constructor(protected key: Key) {}
+
+  public comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key);
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +48,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
